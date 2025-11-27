084dd6c97ac6deb376b99c789bcde30b87f44c38
y4voRst <s20070918@gmail.com>
2025-11-28 00:03:52 +0300

    Update laba10.py

--- laba10.py
+++ laba10.py
@@ -1,3 +1,4 @@
+# 1-й
 def getMtrx(name):
     matr = []
     with open(name, "r") as f:
@@ -15,3 +16,27 @@ print("строка с макс числом: " + str(st) + "сумма стро
 
 f = open("Попов Семён Максимович_УБ-51_vivod.txt", 'w', encoding="utf-8")
 f.write("строка с макс числом: " + str(st) + "сумма строки: " + str(maxSum))
+# 2-й
+def getMtrx(name):
+    matr = []
+    with open(name, "r") as f:
+        for line in f:
+            row = [int(x) for x in line.split()]
+            matr.append(row)
+    return matr
+
+a = getMtrx("Попов Семён Максимович_УБ-51_vvod2.txt")
+
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i][j]<0: a[i][j]=0
+        elif a[i][j]>0: a[i][j]=1
+
+st = ''
+for i in range(len(a)):
+    s = ''
+    for j in range(0, i+1):
+        s += str(a[i][j])
+    st += s + '\n'
+f = open("Вывод2.txt", "w")
+f.write(st)
